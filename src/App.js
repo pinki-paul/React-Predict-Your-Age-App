11cff7c963b51543d5fcef8732897e205339fad3
Pinki Paul <pinkipaul371@gmail.com>
2024-02-27 15:04:59 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const [name, setName] = useState("");
+  const [predictedAge, setPredictedAge] = useState(null);
+
+
+  const fetchAge = () => {
+    Axios.get(`https://api.agify.io/?name=${name}`).then((res) => {
+      setPredictedAge(res.data);
+    });
+  }
+
+  useEffect(() => {
+    fetchAge();
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main-container'>
+      <h1>Prediction of Your Age</h1>
+      <div className='container'>
+        <input placeholder='your name' type='text' onChange={(e) => {
+          setName(e.target.value);
+        }} />
+        <button onClick={fetchAge}>get your age</button>
+        <h3>Your Name: {predictedAge?.name} </h3>
+        <h5>Your Age: {predictedAge?.age} </h5>
+        <p>Your Age Count: {predictedAge?.count} </p>
+      </div>
     </div>
+
+    //predictedAge er initial value "null" tai "?" lagiye trpor "." use kre name age count etc k fetch krte hbe.
   );
 }
 
